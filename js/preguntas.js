40f3742d5f83fe18ef65d3dab1096d6e7a0c9b5b
Lionel-1993 <lionellopez1993@gmail.com>
2021-07-13 19:26:59 -0300

    ...

---
+++ js/preguntas.js
@@ -0,0 +1,12 @@
+ //var modo = confirm("¡queres ver el sitio oscuro?")
+
+ //if (modo) {
+   //  servicio = document.getElementById(servicio)
+     //servicio.style.backgroundColor = "black"
+
+     //var botones = document.getElementsByTagName("")
+ 
+
+     function bgChange(bg) {
+        document.body.style.background = bg;
+    }
